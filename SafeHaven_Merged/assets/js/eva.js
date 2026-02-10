9b3297b6323e793d593d09a06e77ae4996901193
Yramned <s2402357@usls.edu.ph>
2026-02-10 18:23:27 +0800

    Delete SafeHaven_Merged directory

--- SafeHaven_Merged/assets/js/eva.js
+++
@@ -1,9 +0,0 @@
-// Display current date and time
-function updateTime() {
-    const now = new Date();
-    document.getElementById("lastUpdate").textContent =
-        now.toLocaleString();
-}
-
-updateTime();
-setInterval(updateTime, 60000);
