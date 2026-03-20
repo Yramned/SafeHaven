5c66c8c0b3a7d85afc2ed5b239d473b287acf66f
Yramned <s2402357@usls.edu.ph>
2026-03-20 20:16:22 +0800

    tesefndended

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SafeHaven - Main Configuration File
- * Auto-detects environment (localhost vs HelioHost)dadwd
+ * Auto-detects environment (localhost vs HelioHost)dadwdfsefse
  */
 
 // Detect environment
